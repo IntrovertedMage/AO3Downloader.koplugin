fbc95018c91edbb9539ac628e1b98716b0b8fae5
Renee Ibeji <reneeibeji@gmail.com>
2025-04-28 19:27:21 +0100

    Update description to specify fanfiction source as Archive Of Our Own

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     name = "fanfic",
     fullname = _("AO3 downloader"),
-    description = _([[Browse for and download fanfiction]]),
+    description = _([[Browse for and download fanfiction from Archive Of Our Own]]),
 }
